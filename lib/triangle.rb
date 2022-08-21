91688518ae7db374b55d9bdaa11437d2c159dce5
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-08-21 16:39:55 +0300

    commit tree 0a56030f3a872461fd5629fc2f7a6c0dbb5e1fc5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind
+    validity
+    if a == b && b== c
+      :equilateral
+    elsif
+      a == b || b == c || a == c
+      :isosceles
+    else
+       
+        :scalene
+      end
+    end
+
+    def inequality?
+      a + b > c && a + c > b && b + c > a
+    end
+    def postiv?
+      [a, b, c].all?(&:positive?) 
+    end
+      def validity 
+        raise TriangleError unless postiv? && inequality?
+    end
+    class TriangleError <StandardError
+    end
+   
 end
